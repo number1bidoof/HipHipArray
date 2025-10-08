67a0275bb0b74a797fb7b13bf0a6448edb4f1ba7
Ayah Abdalla <anabdalla1@my.waketech.edu>
2025-10-08 13:40:15 +0000

    BAM

--- EvenOddSplit.java
+++ EvenOddSplit.java
@@ -1,53 +1,61 @@
+/*
+Name: Ayah Abdalla
+Date: 10/03/2025
+Description: Splits an array of 15 numbers by even or odd
+*/
 
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class EvenOddSplit {
     public static int[] getEvens(int[] nums) {
-        int countEvens = 0;
-        int[] evenNum = new int[nums.length];
-        for (int i : nums) {
-            if (i % 2 == 0) {
-                evenNum[countEvens++] = i;
+        int countEvens = 0; // index value
+        int[] evenNum = new int[nums.length]; // makes an empty list for the even numbers
+        for (int number : nums) {
+            if (number % 2 == 0) { // if the number is even
+                evenNum[countEvens++] = number; // adds number to the array, increments by one for the next index
             }
         }
-        return evenNum;
+        return evenNum; // returns the array of even number
     }
 
     public static int[] getOdds(int[] nums) {
-        int countOdds = 0;
-        int[] oddNum = new int[nums.length];
-        for (int i : nums) {
-            if (i % 2 != 0) {
-                oddNum[countOdds++] = i;
+        int countOdds = 0; // index value
+        int[] oddNum = new int[nums.length]; // makes an empty list for the odd numbers
+        for (int number : nums) {
+            if (number % 2 != 0) { // if the number is odd
+                oddNum[countOdds++] = number; // adds number to the array, increments by one for the next index
             }
         }
-        return oddNum;
+        return oddNum; // returns the array of odd numbers
+    }
+    public static int[] formatOutput(int[] nums){ // for readability and better interface
+        for(int i : nums){ // for each number in the array
+            if(i != 0) { // if it is not 0 (meaning the value didnt get filled in)
+            System.out.print(i + " ");  // prints it out
+            }
+        }
+        System.out.println(" ");
+        return nums; // returns the array
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
         System.out.println("Enter 15 integers with one space between each integer:");
         int[] userList;
-        userList = new int[15];
-        for (int i = 0; i < userList.length; i++) {
-            userList[i] = scanner.nextInt();
+        userList = new int[15]; // is a set number
+        for (int i = 0; i < userList.length; i++) { 
+            userList[i] = scanner.nextInt(); // assigns the number to an index
         }
-        int[] evens = getEvens(userList);
-        int[] odds = getOdds(userList);
-        System.out.print("Even numbers: ");
-        for (int j : evens) {
-            if (j != 0) {
-                System.out.print(j + " ");
-            }
-        }
-        System.out.println(" ");
-        System.out.print("Odd numbers: ");
-        for(int h: odds){
-            if(h != 0) {
-            System.out.print(h + " ");
-            }
-        }
+        int[] evens = getEvens(userList); // seperate array for even numbers
+        int[] odds = getOdds(userList); // seperate array for odd numbers
+
+        System.out.print("Even Numbers: ");
+        formatOutput(evens); // formats the output
+
+        System.out.print("Odd Numbers: ");
+        formatOutput(odds); // formats the output
+
+        scanner.close(); // closes the scanner
     }
 }
--- ShufflePlaylist.java
+++ ShufflePlaylist.java
@@ -1,20 +1,27 @@
-import java.util.Arrays;
+/*
+Name: Ayah Abdalla
+Date: 10/03/2025
+Description: A method that shuffles a playlist of songs
+*/
+
+import java.lang.Math;
 
 public class ShufflePlaylist {
     public static String[] shuffle(String[] playlist){
-        for (int i = 0; i < playlist.length - 1 ; i++){
-            int j = (int)(Math.random() * playlist.length);
-            String temp = playlist[i];
-            playlist[i] = playlist[j];
-            playlist[j] = temp;
+        for (int i = 0; i < playlist.length - 1 ; i++){ 
+            int j = (int)(Math.random() * playlist.length); // gets a valid random index value for the playlist
+            String temp = playlist[i]; // sets a temporary varrible to the given song of the playlist (based on i)
+            playlist[i] = playlist[j]; // sets the song in the for loop to the random song (based on j)
+            playlist[j] = temp; // swaps the random song to the given song (based on i)
         }
-        for (String song : playlist){
-            System.out.println(song + " ");
-        }
-        return playlist;
+        return playlist; // returns the shuffled playlist
     }
     public static void main(String[] args) {
         String[] playlist = {"Gangnam Style – PSY","I Love You – Barney","Never Gonna Give You Up – Rick Astley", "Shake It Off – Taylor Swift","Baby Shark – Pinkfong","Axel F (Crazy Frog) – Crazy Frog","What Does the Fox Say? – Ylvis"};
-        String[] testing = shuffle(playlist);
+        String[] shuffledPlaylist = shuffle(playlist); // shuffles the playlist
+
+        for (String song : shuffledPlaylist){ // helps format the output 
+            System.out.println(song + " ");
+        }
     }
 }
